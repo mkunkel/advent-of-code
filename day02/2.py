e549ad79d2342fc7eec18adcf66293e3f87e1830
Mike Kunkel <mike@mkunkel.com>
2019-12-02 22:03:18 -0600

    Day 2 work in progress
    Ruby and python complete. Still needing to learn more about elixir
    to progress

--- day02/2.py
+++ day02/2.py
@@ -1,1 +1,38 @@
 #! /usr/bin/env python
+
+def chunks(codes, size):
+    for code in range(0, len(codes), size):
+        yield codes[code:code + size]
+
+def process(n, v):
+    file = open("day02/input.txt", "r")
+    codes = file.read().split(",")
+    codes = map(int, codes)
+
+    codes[1] = n
+    codes[2] = v
+
+    ops = chunks(codes, 4)
+
+    for op in ops:
+        if op[0] == 1:
+            before = codes[op[3]]
+            codes[op[3]] = codes[op[1]] + codes[op[2]]
+        elif op[0] == 2:
+            codes[op[3]] = codes[op[1]] * codes[op[2]]
+        elif op[0] == 99 and op[1] <= 0:
+            return codes[0]
+    return codes[0]
+
+
+numbers = range(0, 99)
+
+for noun in numbers:
+    for verb in numbers:
+        value = process(noun, verb)
+
+        if value == 19690720:
+            print "Value: %s" % value
+            print "Noun: %s" % noun
+            print "Verb: %s" % verb
+            print "Total: %s" % str(100 * noun + verb)
--- day02/2.rb
+++ day02/2.rb
@@ -1,38 +1,32 @@
 #! /usr/bin/env ruby
-input = File.read('day02/input.txt').split(',').map(&:to_i)
+def process(n, v)
+  input = File.read('day02/input.txt').split(',').map(&:to_i)
 
-input[1] = 12
-input[2] = 2
-require 'pry'
-input.each_slice(4) do |slice|
-  code, noun, verb, pos = slice
-  case code
-  when 99
-    puts input[0]
-    puts "SLICE #{slice}"
-    exit 0 if slice.length == 1
-  when 1
-    binding.pry if input[noun] + input[verb] == 19690720
-    input[pos] = input[noun] + input[verb]
-  when 2
-    binding.pry if input[noun] + input[verb] == 19690720
-    input[pos] = input[noun] * input[verb]
+  input[1] = n
+  input[2] = v
+
+  input.each_slice(4) do |slice|
+    case slice[0]
+    when 99
+      return input[0]
+    when 1
+      input[slice[3]] = input[slice[1]] + input[slice[2]]
+    when 2
+      input[slice[3]] = input[slice[1]] * input[slice[2]]
+    end
   end
-  puts input[0]
 end
 
-puts 100 * noun + verb
-
-numbers = (0.99).to_a
+numbers = (0..99).to_a
 
 numbers.each do |noun|
   numbers.each do |verb|
-    value = noun * verb
-    break if value = 19690720
+    value = process(noun, verb)
+    if value == 19690720
+      puts "Value: #{value}"
+      puts "Noun: #{noun}"
+      puts "Verb: #{verb}"
+      puts "Total: #{100 * noun + verb}"
+    end
   end
 end
-
-puts "value: #{value}"
-puts "noun: #{noun}"
-puts "verb: #{verb}"
-puts "total: #{100 * noun + verb}"
