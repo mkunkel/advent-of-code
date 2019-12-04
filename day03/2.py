4c089c07f296eaec33c92426297d1f7e4656f9c8
Mike Kunkel <mike@mkunkel.com>
2019-12-03 21:49:43 -0600

    Day 3 complete for ruby and python
    Skipping elixir until I get a better understanding

--- day03/2.py
+++ day03/2.py
@@ -1,1 +1,51 @@
 #! /usr/bin/env python
+from operator import methodcaller
+
+def to_dict(move):
+    return {
+        'direction': move[0],
+        'distance': int(move[1:])
+    }
+
+def to_directions(moves):
+    return to_dict(moves)
+
+def to_path(directions):
+    velocity = {
+    'R': [1, 0],
+    'L': [-1, 0],
+    'U': [0, 1],
+    'D': [0, -1]
+    }
+    coords = [[0, 0]]
+    for move in directions:
+        for _ in range(move['distance']):
+            coord = [x + y for x, y in zip(velocity[move['direction']], coords[-1])]
+            coords.append(coord)
+    return coords
+
+def to_paths(line):
+    line = line.split(',')
+    directions = map(to_directions, line)
+    return to_path(directions)
+
+def intersection(lst):
+    tup1 = map(tuple, lst[0])
+    tup2 = map(tuple, lst[1])
+    return list(map(list, set(tup1).intersection(tup2)))
+
+def distance(coords, paths):
+    return paths[0].index(coords) + paths[1].index(coords)
+
+def closest(paths):
+    common = intersection(paths)
+    common.remove([0, 0])
+    return min([distance(x, paths) for x in common])
+
+file = open("day03/input.txt", "r")
+lines = file.read().splitlines()
+file.close()
+
+paths = map(to_paths, lines)
+
+print closest(paths)
--- day03/2.rb
+++ day03/2.rb
@@ -1,1 +1,32 @@
 #! /usr/bin/env ruby
+lines = File.read('day03/input.txt').split("\n").map { |line| line.split(',') }
+
+lines.map! do |line|
+  line.map do |l|
+    {
+      direction: l[0],
+      distance: l[1..-1].to_i
+    }
+  end
+end
+
+directions = {
+  'R' => [1, 0],
+  'L' => [-1, 0],
+  'U' => [0, 1],
+  'D' => [0, -1]
+}
+
+paths = lines.map do |line|
+  coords = [[0, 0]]
+  line.each do |move|
+    move[:distance].times do
+      coords << [coords.last, directions[move[:direction]]].transpose.map {|x| x.reduce(:+)}
+    end
+  end
+  coords
+end
+
+common = (paths[0] & paths[1]).reject { |coords| coords == [0, 0] }
+
+puts common.map { |coords| paths[0].index(coords) + paths[1].index(coords) }.min
