265d2e7746ff687a8700c396f8e68d010a20f097
Mike Kunkel <mike@mkunkel.com>
2019-12-10 13:09:05 -0600

    Finish day 8 in python

--- day08/1.py
+++ day08/1.py
@@ -1,1 +1,23 @@
 #! /usr/bin/env python3
+def sort_order(val):
+    # print(val)
+    return val.count('0')
+
+def divide_chunks(lst, length):
+    # looping till length l
+    for i in range(0, len(lst), length):
+        yield lst[i:i + length]
+
+
+file = open("day08/input.txt", "r")
+input = file.read().rstrip()
+file.close()
+
+layers = list(divide_chunks([char for char in input], 25 * 6))
+# layers = list(divide_chunks(lines, 6))
+
+layers.sort(key = sort_order)
+layers = list(layers)
+checksum = layers[0].count('1') * layers[0].count('2')
+
+print(checksum)
--- day08/2.py
+++ day08/2.py
@@ -1,1 +1,38 @@
 #! /usr/bin/env python3
+#! /usr/bin/env python3
+def sort_order(val):
+    # print(val)
+    return val.count('0')
+
+def divide_chunks(lst, length):
+    # looping till length l
+    for i in range(0, len(lst), length):
+        yield lst[i:i + length]
+
+def color_mapping(number_string):
+    return { '0': '█', '1': '▒', '2': ' ' }[number_string]
+
+file = open("day08/input.txt", "r")
+input = file.read().rstrip()
+file.close()
+
+layers = list(divide_chunks([char for char in input], 25 * 6))
+chars = [list(a) for a in zip(*layers)]
+
+
+# rows = [[] for _i in range(6)]
+
+# for layer in layers:
+    # for row in layer:
+output = []
+
+for char in chars:
+    for c in char:
+        if c != '2':
+            output.append(color_mapping(c))
+            break
+
+rows = list(divide_chunks([char for char in output], 25))
+
+for row in rows:
+    print(''.join(row))
