9249bd957a42aef2aa8171400213a0830ff90cb9
Mike Kunkel <mike@mkunkel.com>
2020-12-02 09:57:02 -0600

    add execution time to day 1

--- 2020/day01/1.rb
+++ 2020/day01/1.rb
@@ -1,7 +1,10 @@
 #! /usr/bin/env ruby
 
 input = File.read('2020/day01/input.txt').split.map(&:to_i)
-
+start = Time.new
 additions = input.map { |i| 2020 - i }
+finish = Time.new
+
 
 puts (input & additions).reduce(:*)
+puts "Time to run: #{(finish - start) * 1000000}µs" # convert to microseconds
--- 2020/day01/2.rb
+++ 2020/day01/2.rb
@@ -2,6 +2,11 @@
 
 input = File.read('2020/day01/input.txt').split.map(&:to_i)
 
+start = Time.new
 values = input.combination(3).to_a.find { |combo| combo.reduce(:+) == 2020 }
+finish = Time.new
 
-puts values.reduce(:*)
+output = values.reduce(:*)
+
+puts output
+puts "Time to run: #{(finish - start) * 1000000}µs" # convert to microseconds
