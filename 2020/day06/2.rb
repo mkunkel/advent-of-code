a81a84104bc0a63a7915145457ae894e71aeb978
Mike Kunkel <mike@mkunkel.com>
2020-12-06 13:38:10 -0600

    day 6

---
+++ 2020/day06/2.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/env ruby
+start = Time.new
+class Array
+  def split(value = '')
+    arr = dup
+    result = []
+    while (idx = arr.index(value))
+      result << arr.shift(idx)
+      arr.shift
+    end
+    result << arr
+  end
+end
+
+groups = File.read('2020/day06/input.txt').split("\n").split
+
+counts = groups.map do |group|
+  group.map(&:chars).reduce(&:&).count
+end
+
+puts counts.reduce(&:+)
+
+finish = Time.new
+puts "Time to run: #{(finish - start) * 1000}ms" # convert to microseconds
