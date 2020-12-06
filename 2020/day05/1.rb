8964bdb741924333bf1112e6cf3895e5c9c3767b
Mike Kunkel <mike@mkunkel.com>
2020-12-06 13:38:00 -0600

    day 5

---
+++ 2020/day05/1.rb
@@ -0,0 +1,44 @@
+#! /usr/bin/env ruby
+start = Time.new
+
+class BoardingPass
+  def initialize(value)
+    @value = value
+  end
+
+  def parse
+    rows = (0..127).to_a
+    cols = (0..7).to_a
+    @value.chars.each do |letter|
+      case letter
+      when 'F'
+        rows = rows.shift(rows.length / 2)
+      when 'B'
+        rows = rows.pop(rows.length / 2)
+      when 'L'
+        cols = cols.shift(cols.length / 2)
+      when 'R'
+        cols = cols.pop(cols.length / 2)
+      end
+    end
+    @boarding_pass = { row: rows.first, col: cols.first }
+  end
+
+  def seat_id
+    @boarding_pass[:row] * 8 + @boarding_pass[:col]
+  end
+end
+
+lines = File.read('2020/day05/input.txt').split("\n")
+
+ids = lines.map do |line|
+  pass = BoardingPass.new(line)
+  pass.parse
+  pass.seat_id
+end
+
+puts ids.max
+
+
+finish = Time.new
+puts "Time to run: #{(finish - start) * 1000}ms" # convert to microseconds
