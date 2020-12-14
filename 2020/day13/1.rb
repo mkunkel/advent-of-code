fe36f55159277e715be342c78617027b518b0c29
Mike Kunkel <mike@mkunkel.com>
2020-12-14 16:11:49 -0600

    day 13
    part 2 is WIP

--- 2020/day13/1.rb
+++ 2020/day13/1.rb
@@ -2,4 +2,11 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2020/day13/input.txt').split("\n")
+time, bus_ids = File.read('2020/day13/input.txt').split("\n")
+bus_ids = bus_ids.split(',').reject { |bus_id| bus_id == 'x' }
+times = bus_ids.map { |bus_id| bus_id.to_i - (time.to_i % bus_id.to_i) }
+
+departs = times.min
+bus = bus_ids[times.index(departs)].to_i
+
+puts bus * departs
--- 2020/day13/2.rb
+++ 2020/day13/2.rb
@@ -1,5 +1,56 @@
 #! /usr/bin/env ruby
 require 'awesome_print'
 require 'pry'
+require 'prime'
 
-lines = File.read('2020/day13/input.txt').split("\n")
+def depart(busses, increment)
+  start = 0
+  final_departure = nil
+  timestamp = start
+
+  until final_departure do
+    final_departure = timestamp if busses.all? { |b| (timestamp + b[:index]) % b[:bus] == 0 }
+    timestamp += increment
+  end
+
+  puts final_departure
+  final_departure
+end
+
+_time, bus_ids = File.read('2020/day13/input.txt').split("\n")
+bus_ids = bus_ids.split(',').map { |bus_id| bus_id == 'x' ? 'x' : bus_id.to_i }
+
+modified = bus_ids.map.with_index do |bus_id, index|
+  if bus_id == 'x'
+    'x'
+  else
+    { bus: bus_id, index: index, factors: bus_id.prime_division.map(&:first) }
+  end
+end
+
+modified.reject! { |mod| mod == 'x' }
+modified.sort! { |a, b| b[:bus] <=> a[:bus] }
+# min = 100000000000000
+# start = (min / head[:bus]).floor * head[:bus]
+# binding.pry
+increment = modified.first[:bus]
+
+
+busses = []
+increment = 1
+
+modified.each do |bus|
+  busses << bus
+  increment = depart(busses, increment)
+end
+
+  puts increment
+
+# mods = modified.map { |mod| mod[:bus] }
+# remainders = modified.map { |mod| mod[:index] }
+# modulo = remainders.reduce(&:*)
+# modulos = mods.map { |mod| modulo / mod }
+
+# # puts mods.to_s
+# # puts remainders.to_s
+# # puts chinese_remainder(mods, remainders)
