d3a00c55d948e6ed36fc33a8f101eb3fa1a36577
Mike Kunkel <mike@mkunkel.com>
2020-12-14 18:45:22 -0600

    Day 14

--- 2020/day14/1.rb
+++ 2020/day14/1.rb
@@ -2,4 +2,46 @@
 require 'awesome_print'
 require 'pry'
 
+class Bitmasker
+  DEFAULT_VALUE = '000000000000000000000000000000000000'
+
+  def initialize(lines)
+    @lines = lines
+    @memory = []
+  end
+
+  def apply_mask(value)
+    string = value.to_i.to_s(2).rjust(36, '0')
+    chars = string.chars
+    @mask.chars.map.with_index do |char, index|
+      char == 'X' ? chars[index] : char
+    end.join
+  end
+
+  def update_mask(string)
+    @mask = string.gsub('mask = ', '')
+  end
+
+  def process
+    @lines.each do |line|
+      if line.length > DEFAULT_VALUE.length
+        update_mask(line)
+      else
+        position, value = line.match(/mem\[(\d+)\] = (\d+)/).captures
+        @memory[position.to_i] = apply_mask(value)
+      end
+    end
+  end
+
+  def total
+    @memory.map { |value| value.nil? ? 0 : value.to_i(2) }.reduce(&:+)
+  end
+end
+
+
+
 lines = File.read('2020/day14/input.txt').split("\n")
+
+bitmasker = Bitmasker.new(lines)
+bitmasker.process
+puts bitmasker.total
--- 2020/day14/2.rb
+++ 2020/day14/2.rb
@@ -2,4 +2,63 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2020/day14/input.txt').split("\n")
+class Bitmasker
+  DEFAULT_VALUE = '000000000000000000000000000000000000'
+
+  def initialize(file_path)
+    @file_path = file_path
+    @memory = {}
+  end
+
+  def apply_mask(value)
+    string = value.to_i.to_s(2).rjust(36, '0')
+    chars = string.chars
+    new_mask = @mask.chars.map.with_index do |char, index|
+      char == '0' ? chars[index] : char
+    end.join
+    permutations(new_mask)
+  end
+
+  def permutations(string)
+    floating_count = string.count('X')
+    options = ['0', '1'].repeated_permutation(floating_count)
+    options.map do |option|
+      new_string = string.dup
+      floating_count.times do |time|
+        index = new_string.index('X')
+        new_string[index] = option.shift
+      end
+      new_string.to_i(2)
+    end
+  end
+
+  def update_mask(string)
+    @mask = string.gsub('mask = ', '')
+  end
+
+  def process
+    File.readlines(@file_path).each do |line|
+      if line.length > DEFAULT_VALUE.length
+        update_mask(line)
+      else
+        location, value = line.match(/mem\[(\d+)\] = (\d+)/).captures
+        positions = apply_mask(location)
+        positions.each do |position|
+          @memory[position] = value.to_i
+        end
+      end
+    end
+  end
+
+  def total
+    @memory.values.reduce(&:+)
+  end
+end
+
+
+
+file_path = '2020/day14/input.txt'
+
+bitmasker = Bitmasker.new(file_path)
+bitmasker.process
+puts bitmasker.total
