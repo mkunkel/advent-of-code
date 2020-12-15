9dcde606faef30ba0a1dc4c61cdb0e0e0b54d53e
Mike Kunkel <mike@mkunkel.com>
2020-12-15 15:05:01 -0600

    day 15

--- 2020/day15/1.rb
+++ 2020/day15/1.rb
@@ -2,4 +2,13 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2020/day15/input.txt').split("\n")
+numbers = File.read('2020/day15/input.txt').split(',').map(&:to_i)
+
+until numbers.length == 2020 do
+  number = numbers.last
+  indices = numbers.map.with_index { |num, index| num == number ? index : nil }.compact
+  value = indices.count == 1 ? 0 : indices[-1] - indices[-2]
+  numbers << value
+end
+
+puts numbers.last
--- 2020/day15/2.rb
+++ 2020/day15/2.rb
@@ -2,4 +2,23 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2020/day15/input.txt').split("\n")
+numbers = File.read('2020/day15/input.txt').chomp.split(',')
+
+indices = Hash.new { |h,k| h[k] = [] }
+numbers.each_with_index do |number, index|
+  indices[number.to_i] << index
+  indices[:last] = number
+end
+
+index = numbers.count
+until index == 30000000 do
+  number = indices[:last].to_i
+  count = indices[number].count
+  value = count == 1 ? 0 : indices[number][-1] - indices[number][-2]
+  indices[value] << index
+  indices[value] = indices[value].last(2)
+  indices[:last] = value
+  index += 1
+end
+
+puts indices[:last]
