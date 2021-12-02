213be91354b5dd5a9784894eab93bf174d744e29
Mike Kunkel <mike@mkunkel.com>
2021-12-01 18:51:26 -0600

    Day 1

--- 2021/day01/1.rb
+++ 2021/day01/1.rb
@@ -3,3 +3,9 @@ require 'awesome_print'
 require 'pry'
 
 lines = File.read('2021/day01/input.txt').split("\n")
+
+changes = lines.map.with_index do |line, i|
+  line.to_i > lines[i - 1].to_i ? 'increased' : 'decreased'
+end[1..-1]
+
+puts changes.count { |change| change == 'increased' }
--- 2021/day01/2.rb
+++ 2021/day01/2.rb
@@ -2,4 +2,14 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2021/day01/input.txt').split("\n")
+lines = File.read('2021/day01/input.txt').split("\n").map(&:to_i)
+
+depths = lines.map.with_index do |line, i|
+  i < 2 ? 0 : line + lines[i - 1] + lines[i - 2]
+end
+
+changes = depths.map.with_index do |depth, i|
+  i > 0 && depth.to_i > depths[i - 1].to_i ? 'increased' : 'decreased'
+end
+
+puts changes[3..-1].count { |change| change == 'increased' }
