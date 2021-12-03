6711fcaa9f55b7f7a9f7e4fb3108c50d74efe2b7
Mike Kunkel <mike@mkunkel.com>
2021-12-03 09:46:22 -0600

    day 2

--- 2021/day02/1.rb
+++ 2021/day02/1.rb
@@ -3,3 +3,20 @@ require 'awesome_print'
 require 'pry'
 
 lines = File.read('2021/day02/input.txt').split("\n")
+
+horizontal = 0
+vertical = 0
+
+lines.each do |line|
+  direction, amount = line.split(' ')
+  case direction
+  when 'forward'
+    horizontal += amount.to_i
+  when 'down'
+    vertical += amount.to_i
+  when 'up'
+    vertical -= amount.to_i
+  end
+end
+
+puts horizontal * vertical
--- 2021/day02/2.rb
+++ 2021/day02/2.rb
@@ -3,3 +3,23 @@ require 'awesome_print'
 require 'pry'
 
 lines = File.read('2021/day02/input.txt').split("\n")
+
+horizontal = 0
+vertical = 0
+aim = 0
+
+lines.each do |line|
+  direction, amount = line.split(' ')
+  case direction
+  when 'forward'
+    horizontal += amount.to_i
+    vertical += amount.to_i * aim
+  when 'down'
+    aim += amount.to_i
+  when 'up'
+    aim -= amount.to_i
+  end
+  # binding.pry
+end
+
+puts horizontal * vertical
