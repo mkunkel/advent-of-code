2d2fb3f719a2b3688146aa7ab906ef39d22586ab
Mike Kunkel <mike@mkunkel.com>
2021-12-05 16:07:16 -0600

    day 5

--- 2021/day05/2.rb
+++ 2021/day05/2.rb
@@ -2,4 +2,60 @@
 require 'awesome_print'
 require 'pry'
 
+def expand_grid(grid, vent)
+  max_x = [vent[:start][0], vent[:end][0]].max
+  max_y = [vent[:start][1], vent[:end][1]].max
+  max_width = [grid.first.length, max_x + 1].max
+  while grid.length < max_y + 1 do
+    grid << []
+  end
+
+  grid.each do |row|
+    while row.length < max_width do
+      row << 0
+    end
+  end
+end
+
+def coord_list(vent)
+  x = [vent[:start][0], vent[:end][0]].uniq.sort
+  y = [vent[:start][1], vent[:end][1]].uniq.sort
+  x = (x[0]..x[1]).to_a if x.length > 1
+  y = (y[0]..y[1]).to_a if y.length > 1
+
+  if x.count == 1 || y.count == 1
+    x.product(y)
+  else
+    x_trajectory = vent[:start][0] > vent[:end][0] ? -1 : 1
+    y_trajectory = vent[:start][1] > vent[:end][1] ? -1 : 1
+    list = [vent[:start]]
+    while list.last != vent[:end] do
+      last = list.last
+      list << [list.last[0] + x_trajectory, list.last[1] + y_trajectory]
+    end
+    list
+  end
+end
+
+def add_vent(grid, vent)
+  coord_list(vent).each { |coord| grid[coord[1]][coord[0]] += 1 }
+end
+
 lines = File.read('2021/day05/input.txt').split("\n")
+vents = lines.map do |line|
+  one, two = line.split(' -> ')
+  one = one.split(',').map(&:to_i)
+  two = two.split(',').map(&:to_i)
+  { start: one, end: two }
+end
+
+grid = [[]]
+
+vents.each do |vent|
+  expand_grid(grid, vent)
+  add_vent(grid, vent)
+end
+
+puts grid.map(&:join).map { |row| row.gsub('0', '.') }
+puts
+puts grid.map { |row| row.count { |int| int > 1 } }.reduce(&:+)
