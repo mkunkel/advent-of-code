f6ebcea6357a9103565d378f977b48c4281d46ce
Mike Kunkel <mike@mkunkel.com>
2021-12-10 10:55:35 -0600

    day 7

--- 2021/day07/1.rb
+++ 2021/day07/1.rb
@@ -2,4 +2,17 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2021/day07/input.txt').split("\n")
+positions = File.read('2021/day07/input.txt').split(',').map(&:to_i)
+
+min = positions.min
+max = positions.max
+
+options = (min..max).to_a
+
+consumed = options.map do |option|
+  positions.map { |pos| (pos - option).abs }.sum
+end
+
+min_index = consumed.index(consumed.min)
+
+puts positions.map { |pos| (pos - options[min_index]).abs }.sum
--- 2021/day07/2.rb
+++ 2021/day07/2.rb
@@ -2,4 +2,21 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2021/day07/input.txt').split("\n")
+def diff(a, b)
+  (1..(a - b).abs).sum
+end
+
+positions = File.read('2021/day07/input.txt').split(',').map(&:to_i)
+
+min = positions.min
+max = positions.max
+
+options = (min..max).to_a
+
+consumed = options.map do |option|
+  positions.map { |pos| diff(pos, option) }.sum
+end
+
+min_index = consumed.index(consumed.min)
+
+puts positions.map { |pos| diff(pos, options[min_index]) }.sum
