39935df9a0c32dbdd601a11bfff2ffb76953c445
Mike Kunkel <mike@mkunkel.com>
2021-12-12 20:24:35 -0600

    day 12

--- 2021/day12/1.rb
+++ 2021/day12/1.rb
@@ -2,4 +2,27 @@
 require 'awesome_print'
 require 'pry'
 
+def traverse(caves, paths = [], path = ['start'])
+  paths << path if path.last == 'end'
+  return paths if path.last == 'end'
+  caves[path.last].each do |option|
+    unless path.include?(option) && /[[:lower:]]/.match(option)
+      new_path = path.dup
+      new_path << option
+      traverse(caves, paths, new_path)
+    end
+  end
+  paths
+end
+
+connections = Hash.new { |h, k| h[k] = [] }
 lines = File.read('2021/day12/input.txt').split("\n")
+lines.map! { |line| line.split('-') }
+lines.each do |from, to|
+  connections[from] << to
+  connections[to] << from
+end
+
+paths = traverse(connections).select { |path| path.include?('end') }
+
+puts paths.count
--- 2021/day12/2.rb
+++ 2021/day12/2.rb
@@ -2,4 +2,43 @@
 require 'awesome_print'
 require 'pry'
 
+def traverse(caves, repeat = nil, paths = [], path = ['start'])
+  paths << path if path.last == 'end'
+  return paths if path.last == 'end'
+  # binding.pry if path == ["start", "A", "b", "A", "b", "A"]
+  caves[path.last].each do |option|
+    if allowed?(path, option, repeat)
+      new_path = path.dup
+      new_path << option
+      traverse(caves, repeat, paths, new_path)
+    end
+  end
+  paths
+end
+
+def allowed?(path, character, repeat_character)
+  return true unless path.include?(character)
+  return true if /[[:upper:]]/.match(character)
+  return false if path.include?(character) && character != repeat_character
+  path.count(repeat_character) < 2
+end
+
+connections = Hash.new { |h, k| h[k] = [] }
 lines = File.read('2021/day12/input.txt').split("\n")
+lines.map! { |line| line.split('-') }
+lines.each do |from, to|
+  connections[from] << to
+  connections[to] << from
+end
+
+small_caves = connections.keys.reject { |key| /[[:upper:]]/.match(key) || key == 'start' || key == 'end' }
+
+paths = []
+
+small_caves.each do |cave|
+  traverse(connections, cave).each { |path| paths << path }
+end
+
+paths.uniq!
+
+puts paths.count
