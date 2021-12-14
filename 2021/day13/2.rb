884378f6fba8b320771547a52686ea753d1e27d7
Mike Kunkel <mike@mkunkel.com>
2021-12-13 22:32:25 -0600

    Day 13

--- 2021/day13/2.rb
+++ 2021/day13/2.rb
@@ -2,49 +2,33 @@
 require 'awesome_print'
 require 'pry'
 
-def fold_horizontal(grid, amount)
-  removed = []
-  should_remove_additional = grid[0].count - amount > amount
-  grid.each do |row|
-    removed << row.pop(amount)
-    row.pop if should_remove_additional
+def fold_horizontal(points, amount)
+  points.map do |point|
+    point[0] > amount ? [(amount * 2) - point[0], point[1]] : point
   end
-
-  removed.each.with_index do |row, index|
-    positions = (0...row.length).find_all { |i| row[i] == '#' }
-    positions.each { |position| grid[index][-1 - position] = '#' }
-  end
-  grid
 end
 
-def fold_vertical(grid, amount)
-  should_remove_additional = grid.count - amount > amount
-  removed = grid.pop(amount)
-  grid.pop if should_remove_additional
-  removed.each.with_index do |row, index|
-    positions = (0...row.length).find_all { |i| row[i] == '#' }
-    positions.each do |position|
-      grid[-1 - index][position] = '#'
-    end
+def fold_vertical(points, amount)
+  points.map do |point|
+    point[1] > amount ? [point[0], (amount * 2) - point[1]] : point
   end
-  grid
 end
 
 dots, instructions = File.read('2021/day13/input.txt').split("\n\n")
 
 dots = dots.split("\n").map { |dot| dot.split(',').map(&:to_i) }
 
-max_x, max_y = dots.transpose.map { |x| x.max + 1 }
-grid = max_y.times.map { max_x.times.map { '.' } }
-dots.each { |coord| grid[coord[1]][coord[0]] = '#' }
-
 instructions = instructions.split("\n")
+
 instructions.map! { |instruction| instruction.gsub('fold along ', '').split('=') }
 instructions.map! { |instruction| [instruction[0], instruction[1].to_i] }
 
 instructions.each do |instruction|
   method = instruction[0] == 'x' ? :fold_horizontal : :fold_vertical
-  grid = send(method, grid, instruction[1])
+  dots = send(method, dots, instruction[1])
 end
 
-grid.each { |row| puts row.join.gsub('.', ' ') }
+max_x, max_y = dots.transpose.map { |x| x.max + 1 }
+grid = max_y.times.map { max_x.times.map { ' ' } }
+dots.each { |coord| grid[coord[1]][coord[0]] = '#' }
+grid.each { |row| puts row.join }
