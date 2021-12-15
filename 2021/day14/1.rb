21dad443a2f51466e08c71e83fb5dfcc4b6f504a
Mike Kunkel <mike@mkunkel.com>
2021-12-14 20:58:36 -0600

    Day 14

--- 2021/day14/1.rb
+++ 2021/day14/1.rb
@@ -2,4 +2,26 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2021/day14/input.txt').split("\n")
+template, rules = File.read('2021/day14/input.txt').split("\n\n")
+rules = rules.split("\n").each_with_object({}) do |rule, hash|
+  pair, char = rule.split(' -> ')
+  hash[pair] = char
+end
+
+iterations = 10
+iterations.times do
+  polymer = ''
+  (template.length - 1).times do |i|
+    key = "#{template[i]}#{template[i + 1]}"
+    insert = rules[key]
+    polymer << "#{template[i]}#{insert}"
+  end
+  polymer << "#{template[-1]}"
+  template = polymer.dup
+end
+
+counts = template.chars.each_with_object(Hash.new { |h, k| h[k] = 0 }) do |char, hash|
+  hash[char] += 1
+end
+
+puts counts.values.max - counts.values.min
--- 2021/day14/2.rb
+++ 2021/day14/2.rb
@@ -2,4 +2,31 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2021/day14/input.txt').split("\n")
+ITERATIONS = 40
+
+template, rules = File.read('2021/day14/input.txt').split("\n\n")
+
+RULES = rules.split("\n").each_with_object({}) do |rule, hash|
+  pair, char = rule.split(' -> ')
+  hash[pair] = [
+    [pair.chars.first, char].join,
+    [char, pair.chars.last].join
+  ]
+end
+
+counts = template.chars.each_cons(2).each_with_object(Hash.new(0)) do |pair, hash|
+  hash[pair.join] += 1
+end
+
+ITERATIONS.times do |i|
+  counts = counts.each_with_object(Hash.new(0)) do |(k,v), hash|
+    RULES[k].each { |key| hash[key] += v }
+  end
+end
+letters = counts.each_with_object(Hash.new(0)) do |(k,v), hash|
+  hash[k.chars[0]] += v
+end
+
+letters[template.chars.last] += 1
+
+ puts letters.values.max - letters.values.min
