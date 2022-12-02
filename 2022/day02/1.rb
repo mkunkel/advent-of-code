b1981b9afa9546db28513ed35e6d53077d27b6cc
Mike Kunkel <mike@mkunkel.com>
2022-12-02 09:52:57 -0600

    Day 2

--- 2022/day02/1.rb
+++ 2022/day02/1.rb
@@ -2,4 +2,30 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day02/input.txt').split("\n")
+plays = File.read('2022/day02/input.txt').split("\n")
+
+# Create arrays to turn plays into index numbers for later comparison
+his = %w(A B C)
+mine = %w(X Y Z)
+
+plays = plays.map do |play|
+  him, me = play.split(' ')
+  { me: me, him: him }
+end
+
+# This array will later be used for applying the result of modulo to determine score
+scores = [3, 6, 0]
+
+results = plays.map do |play|
+  my_index = mine.index(play[:me])
+  his_index = his.index(play[:him])
+
+  # The score for the option selected is 1 higher than the index in the array
+  selected = my_index + 1
+
+  # Compare plays and convert to an index that gives our score
+  resulting_index = (my_index - his_index) % 3
+  selected + scores[resulting_index]
+end
+
+puts results.sum
--- 2022/day02/2.rb
+++ 2022/day02/2.rb
@@ -2,4 +2,40 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day02/input.txt').split("\n")
+plays = File.read('2022/day02/input.txt').split("\n")
+# Create arrays to turn plays into index numbers for later comparison
+options = %w(A B C)
+
+# The index values of this array represent how to change the index of his play
+# to achieve the desired result
+modifiers = %w(Y Z X)
+
+plays = plays.map do |play|
+  him, me = play.split(' ')
+  # Alter my play to create the result desired
+  modifier = modifiers.index(me)
+  his_index = options.index(him)
+  me = options[(his_index + modifier) % 3]
+  { me: me, him: him }
+end
+
+
+# Now score just like in part 1, but
+
+# This array will later be used for applying the result of modulo to determine score
+scores = [3, 6, 0]
+
+results = plays.map do |play|
+  # binding.pry
+  my_index = options.index(play[:me])
+  his_index = options.index(play[:him])
+
+  # The score for the option selected is 1 higher than the index in the array
+  selected = my_index + 1
+
+  # Compare plays and convert to an index that gives our score
+  resulting_index = (my_index - his_index) % 3
+  selected + scores[resulting_index]
+end
+
+puts results.sum
