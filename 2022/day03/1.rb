2443b28c99c7e840403cc79a71d25c6855054501
Mike Kunkel <mike@mkunkel.com>
2022-12-05 10:12:24 -0600

    Day 3

--- 2022/day03/1.rb
+++ 2022/day03/1.rb
@@ -2,4 +2,28 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day03/input.txt').split("\n")
+class Rucksack
+  # use nil to offset the array, since no items have priority 0
+  PRIORITIES = [nil] + ('a'..'z').to_a + ('A'..'Z').to_a
+
+  def initialize(items)
+    @items = items
+    compartmentalize
+  end
+
+  def compartmentalize
+    @compartments ||= @items.chars.each_slice(@items.length / 2).to_a
+  end
+
+  def errors
+    @compartments[0] & @compartments[1]
+  end
+
+  def sum
+    errors.map { |error| PRIORITIES.index(error) }.uniq.sum
+  end
+end
+
+rucksacks = File.read('2022/day03/input.txt').split("\n").map { |rucksack| Rucksack.new(rucksack) }
+
+puts rucksacks.map { |rucksack| rucksack.sum }.sum
--- 2022/day03/2.rb
+++ 2022/day03/2.rb
@@ -2,4 +2,22 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day03/input.txt').split("\n")
+class Group
+  PRIORITIES = [nil] + ('a'..'z').to_a + ('A'..'Z').to_a
+
+  def initialize(content_groups)
+    @content_groups = content_groups.map(&:chars)
+  end
+
+  def common
+    @content_groups.reduce(:&).first
+  end
+
+  def sum
+    PRIORITIES.index(common)
+  end
+end
+
+groups = File.read('2022/day03/input.txt').split("\n").each_slice(3).map { |group| Group.new(group) }
+
+puts groups.map { |group| group.sum }.sum
