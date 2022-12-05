7e92bba835ef01abf0382f997a4a846cf3720882
Mike Kunkel <mike@mkunkel.com>
2022-12-05 10:45:59 -0600

    Day 4

--- 2022/day04/2.rb
+++ 2022/day04/2.rb
@@ -2,4 +2,12 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day04/input.txt').split("\n")
+pairs = File.read('2022/day04/input.txt').split("\n").map { |pair| pair.split(',') }
+
+pairs.map! { |pair| pair.map { |elf| elf.split('-') }.map { |ids| (ids[0]..ids[1]).to_a } }
+
+pairs.map! { |pair| pair.sort { |a, b| a.count <=> b.count } }
+
+fully_contained = pairs.select { |pair| pair[0].any? { |section| pair[1].include?(section) } }
+
+puts fully_contained.count
