845a427135338842d9d81957ca0826c9186fc819
Mike Kunkel <mike@mkunkel.com>
2022-12-06 10:44:52 -0600

    Day 6

--- 2022/day06/2.rb
+++ 2022/day06/2.rb
@@ -2,4 +2,18 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day06/input.txt').split("\n")
+MARKER_LENGTH = 14
+
+chars = File.read('2022/day06/input.txt').chars
+
+marker = nil
+i = 0
+
+until marker do
+  potential_marker = chars[i, MARKER_LENGTH]
+  marker = potential_marker if potential_marker.uniq == potential_marker
+  i += 1 unless marker
+end
+
+puts marker.join
+puts i + MARKER_LENGTH
