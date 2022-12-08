d360ffa574b3776555cb30527aafae82e45b183d
Mike Kunkel <mike@mkunkel.com>
2022-12-08 09:51:22 -0600

    Day 8

--- 2022/day08/1.rb
+++ 2022/day08/1.rb
@@ -2,4 +2,47 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day08/input.txt').split("\n")
+def at_edge?(array, row, col)
+  return true if row == 0 || col == 0
+  row == array.count - 1 || col == array.first.count - 1
+end
+
+def in_bounds?(array, row, col)
+  return false if row < 0 || col < 0
+  row < array.count && col < array.first.count
+end
+
+def visible?(array, row, col)
+  return true if at_edge?(array, row, col)
+  tree = array[row][col]
+  trajectories = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+  trajectories.any? do |y, x|
+    visible = true
+    r = row
+    c = col
+    while in_bounds?(array, r, c) do
+      r += y
+      c += x
+      if !in_bounds?(array, r, c)
+        break
+      elsif array[r][c] >= tree
+        visible = false
+        break
+      end
+    end
+    visible
+  end
+end
+
+trees = File.read('2022/day08/input.txt').split("\n").map { |line| line.chars.map(&:to_i) }
+
+visible = 0
+
+trees.each.with_index do |row, row_i|
+  row.each.with_index do |tree, col_i|
+    visible += 1 if visible?(trees, row_i, col_i)
+  end
+end
+
+puts visible
--- 2022/day08/2.rb
+++ 2022/day08/2.rb
@@ -2,4 +2,45 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day08/input.txt').split("\n")
+def in_bounds?(array, row, col)
+  return false if row < 0 || col < 0
+  row < array.count && col < array.first.count
+end
+
+def score(array, row, col)
+  tree = array[row][col]
+  trajectories = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+  trajectories.map do |y, x|
+    visible = 0
+    r = row
+    c = col
+    while in_bounds?(array, r, c) do
+      r += y
+      c += x
+      if !in_bounds?(array, r, c)
+        break
+      else
+        if array[r][c] >= tree
+          visible += 1
+          break
+        else
+          visible += 1
+        end
+      end
+    end
+    visible
+  end.reduce(&:*)
+end
+
+trees = File.read('2022/day08/input.txt').split("\n").map { |line| line.chars.map(&:to_i) }
+
+
+
+scores = trees.map.with_index do |row, row_i|
+  row.map.with_index do |tree, col_i|
+    score(trees, row_i, col_i)
+  end
+end
+
+puts scores.map { |score| score.max }.max
