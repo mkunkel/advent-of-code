012014e748ac71f9a0225ef6585eed043a817c5e
Mike Kunkel <mike@mkunkel.com>
2022-12-10 14:28:27 -0600

    Day 10

--- 2022/day10/1.rb
+++ 2022/day10/1.rb
@@ -2,4 +2,24 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day10/input.txt').split("\n")
+instructions = File.read('2022/day10/input.txt').split("\n")
+cycle = 1
+x = 1
+signal_strength = 0
+add_next = nil
+
+while cycle < 221 do
+  if add_next
+    x += add_next.to_i
+    add_next = nil
+  else
+    # We only care about the number on addx
+    # noop has no number, so add_next will be nil in that case
+    _instruction, add_next = instructions.shift.split(' ')
+  end
+
+  cycle += 1
+  signal_strength += (x * cycle) if [20, 60, 100, 140, 180, 220].include?(cycle)
+end
+
+puts signal_strength
--- 2022/day10/2.rb
+++ 2022/day10/2.rb
@@ -2,4 +2,24 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day10/input.txt').split("\n")
+instructions = File.read('2022/day10/input.txt').split("\n")
+cycle = 0
+x = 1
+signal_strength = 0
+add_next = nil
+
+while instructions.length > 0 do
+  to_print = [1, 0, -1].include?((cycle % 40) - x) ? '#' : ' '
+  print to_print
+  cycle += 1
+  if add_next
+    x += add_next.to_i
+    add_next = nil
+  else
+    # We only care about the number on addx
+    # noop has no number, so add_next will be nil in that case
+    _instruction, add_next = instructions.shift.split(' ')
+  end
+  puts if cycle % 40 == 0
+end
+puts
