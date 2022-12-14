bcb0463b662a61ac6ab41d1cc20df4a8d01f013b
Mike Kunkel <mike@mkunkel.com>
2022-12-14 09:55:15 -0600

    Day 13 WIP

--- 2022/day13/1.rb
+++ 2022/day13/1.rb
@@ -2,4 +2,35 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2022/day13/input.txt').split("\n")
+def in_order?(pair)
+  left = Array(pair[0])
+  right = Array(pair[1])
+  result = nil
+
+  left.each.with_index do |left_value, i|
+    result = if [left_value, right[i]].any? { |signal| signal.nil? }
+      left_value.nil?
+    elsif left_value.is_a?(Integer) && right[i].is_a?(Integer)
+      compare_values(left_value, right[i])
+    elsif [left_value, right[i]].any? { |signal| signal.is_a?(Array) }
+      in_order?([left_value, right[i]])
+    end
+    return result unless result.nil?
+  end
+  true
+end
+
+def compare_values(left, right)
+  return nil if left == right
+  left < right
+end
+
+pairs = File.read('2022/day13/input.txt').split("\n\n").map do |pair|
+  pair.split("\n").map(&method(:eval))
+end
+
+total = pairs.map.with_index do |pair, i|
+  in_order?(pair) ? i + 1 : 0
+end.sum
+
+puts total
