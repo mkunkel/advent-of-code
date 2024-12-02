b9735a30092f703d312414d7c8e263819606c137
Mike Kunkel <mike@mkunkel.com>
2024-12-02 10:26:39 -0600

    day 1

--- 2024/day01/1.rb
+++ 2024/day01/1.rb
@@ -3,3 +3,14 @@ require 'awesome_print'
 require 'pry'
 
 lines = File.read('2024/day01/input.txt').split("\n")
+amount = lines.count
+lists = lines.map { |line| line.split(/\D+/) }.transpose
+lists = lists.map { |list| list.map(&:to_i).sort }
+
+total = 0
+
+amount.times do |i|
+  total += (lists[0][i] - lists[1][i]).abs
+end
+
+puts total
--- 2024/day01/2.rb
+++ 2024/day01/2.rb
@@ -3,3 +3,12 @@ require 'awesome_print'
 require 'pry'
 
 lines = File.read('2024/day01/input.txt').split("\n")
+lists = lines.map { |line| line.split(/\D+/) }.transpose
+lists = lists.map { |list| list.map(&:to_i) }
+total = 0
+
+lists.first.each_with_index do |num, i|
+  total += num * lists.last.count(num)
+end
+
+puts total
