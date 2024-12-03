a249073f5117734a3a57d25e3834d8a815e40ba2
Mike Kunkel <mike@mkunkel.com>
2024-12-02 18:11:28 -0600

    day 2

--- 2024/day02/1.rb
+++ 2024/day02/1.rb
@@ -3,3 +3,21 @@ require 'awesome_print'
 require 'pry'
 
 lines = File.read('2024/day02/input.txt').split("\n")
+lines.map! { |line| line.split(' ').map(&:to_i) }
+
+total = 0
+lines.each do |line|
+  direction = line[0] < line[1] ? 'up' : 'down'
+  result = 'safe'
+  line.each_cons(2) do |a, b|
+    diff = direction == 'up' ? b - a : a - b
+    if diff < 1 || diff > 3
+      result = 'unsafe'
+      break
+    end
+  end
+
+  total += 1 if result == 'safe'
+end
+
+puts total
--- 2024/day02/2.rb
+++ 2024/day02/2.rb
@@ -3,3 +3,62 @@ require 'awesome_print'
 require 'pry'
 
 lines = File.read('2024/day02/input.txt').split("\n")
+lines.map! { |line| line.split(' ').map(&:to_i) }
+
+total = 0
+
+def is_safe?(line)
+  line.each_cons(2).to_a.all? do |a, b|
+    diff = (a - b).abs
+    diff > 0 && diff < 4
+  end && (line.sort == line || line.sort == line.reverse)
+end
+
+def line_safe?(line)
+  result = is_safe?(line) ? 'safe' : nil
+
+  line.count.times do |i|
+    break if result
+    new_line = line.dup
+    new_line.delete_at(i)
+
+    is_safe = is_safe?(new_line)
+    result = 'safe' if is_safe
+  end
+
+  result || 'unsafe'
+end
+
+lines.each do |line|
+  result = 'safe'
+  if (line.sort == line || line.sort == line.reverse) && line.uniq.count != line.count
+    if line.count - line.uniq.count == 1
+      line = line.uniq
+      result = 'modified'
+    else
+      result = 'unsafe'
+    end
+  elsif (line != line.sort && line != line.sort.reverse) && line.uniq.count != line.count
+    line.count.times do |i|
+      new_line = line.dup
+      new_line.delete_at(i)
+      next if new_line.uniq.count != new_line.count
+      if new_line == new_line.sort || new_line == new_line.sort.reverse
+        line = new_line
+        result = 'modified'
+        break
+      end
+    end
+
+    result = 'unsafe' unless result == 'modified'
+  end
+  result = 'unsafe' if line.uniq.count != line.count
+  result = 'unsafe' unless is_safe?(line) if result == 'modified'
+  result = line_safe?(line) if result == 'safe'
+
+  result = 'safe' if result == 'modified'
+  puts result.upcase
+  total += 1 if result == 'safe'
+end
+
+puts total
