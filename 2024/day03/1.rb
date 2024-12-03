dee4d65ced1ee5c9856cbc96b864e94918f643b3
Mike Kunkel <mike@mkunkel.com>
2024-12-03 12:27:50 -0600

    Day 3

--- 2024/day03/1.rb
+++ 2024/day03/1.rb
@@ -2,4 +2,8 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2024/day03/input.txt').split("\n")
+input = File.read('2024/day03/input.txt').chomp
+
+matches = input.scan(/mul\(\d+,\d+\)/)
+products = matches.map { |match| match.scan(/\d+/).map(&:to_i).reduce(:*) }
+puts products.reduce(:+)
--- 2024/day03/2.rb
+++ 2024/day03/2.rb
@@ -2,4 +2,18 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2024/day03/input.txt').split("\n")
+def strip_donts(input, acc = [])
+  head, tail = input.split(/don't\(\)/, 2)
+  acc << head
+  return acc if tail.nil?
+  head, tail = tail.split(/do\(\)/, 2) unless tail.nil?
+
+  strip_donts(tail, acc)
+end
+
+input = File.read('2024/day03/input.txt').chomp
+parts = strip_donts(input).join
+
+matches = parts.scan(/mul\(\d+,\d+\)/)
+products = matches.map { |match| match.scan(/\d+/).map(&:to_i).reduce(:*) }
+puts products.reduce(:+)
