0329e4016d6526f3429ad47a6158d36bb12afedc
Mike Kunkel <mike@mkunkel.com>
2024-12-04 11:25:50 -0600

    Day 4

--- 2024/day04/1.rb
+++ 2024/day04/1.rb
@@ -2,4 +2,48 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2024/day04/input.txt').split("\n")
+grid = File.read('2024/day04/input.txt').chomp.split("\n").map(&:chars)
+TRAJECTORIES = [-1, 0, 1].repeated_permutation(2).to_a.reject { |coords| coords == [0, 0] }
+ROW_COUNT = grid.count
+COLS_COUNT = grid[0].count
+
+def out_of_bounds?(x, y)
+  x < 0 || y < 0 || x >= COLS_COUNT || y >= ROW_COUNT
+end
+
+def new_coords(x, y, tx, ty)
+  [x + tx, y + ty]
+end
+
+def traverse(grid, coords, trajectory, remaining_letters)
+  count = 0
+  x, y = coords
+  tx, ty = trajectory
+  new_x, new_y = new_coords(x, y, tx, ty)
+  return 0 if out_of_bounds?(new_x, new_y)
+  letter = remaining_letters.shift
+
+  if grid[new_y][new_x] == letter && remaining_letters.length == 0
+    return 1
+  elsif grid[new_y][new_x] == letter
+    count = traverse(grid, [new_x, new_y], trajectory, remaining_letters.dup)
+  end
+
+  count
+end
+
+count = 0
+
+grid.each.with_index do |row, y|
+  row.each.with_index do |cell, x|
+    next unless cell == 'X'
+    TRAJECTORIES.each do |tx, ty|
+      new_x, new_y = new_coords(x, y, tx, ty)
+      next if out_of_bounds?(new_x, new_y)
+
+      count += traverse(grid, [x, y], [tx, ty], 'MAS'.chars)
+    end
+  end
+end
+
+puts count
--- 2024/day04/2.rb
+++ 2024/day04/2.rb
@@ -2,4 +2,28 @@
 require 'awesome_print'
 require 'pry'
 
-lines = File.read('2024/day04/input.txt').split("\n")
+grid = File.read('2024/day04/input.txt').chomp.split("\n").map(&:chars)
+
+WORD = 'MAS'
+examples = [WORD.chars, WORD.reverse.chars].repeated_permutation(2).map do |word1, word2|
+  [
+    [word1[0], '*', word2[0]],
+    ['*', word1[1], '*'],
+    [word2[2], '*', word1[2]]
+  ]
+end
+
+count = 0
+
+grid.each_cons(3).with_index do |rows, y|
+  x = 0
+  while x < rows[0].count - 2
+    set = rows.map { |row| row.slice(x, 3) }
+    [[0, 1], [1, 0], [1, 2], [2, 1]].each { |(y, x)| set[y][x] = '*' }
+
+    count += 1 if examples.include?(set)
+    x += 1
+  end
+end
+
+puts count
